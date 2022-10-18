e181d6dcbae7527a6c55f6c6d0e156fc79b3248d
MK-2000 <57285315+MK-2000@users.noreply.github.com>
2022-10-18 14:01:23 +0530

    exception added

---
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/exception/NutritionPlanNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class NutritionPlanNotFoundException extends Exception{
+
+    public NutritionPlanNotFoundException(String message) {
+        super(message);
+    }
+}
