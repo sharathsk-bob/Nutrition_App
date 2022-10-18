62408614e48fd2ca858d3530681d45230c084906
Ashish19031999 <115981835+Ashish19031999@users.noreply.github.com>
2022-10-18 13:45:12 +0530

    WeightLogNotFoundException added

---
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/exception/WeightLogNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+public class WeightLogNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WeightLogNotFoundException(String message) {
+        super(message);
+	}
+
+}
