8df315952c78b42f4ab13709ebf4a725dd9310fe
MK-2000 <57285315+MK-2000@users.noreply.github.com>
2022-10-18 13:43:53 +0530

    Updated Service file

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/controller/NutritionPlanContoller.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/controller/NutritionPlanContoller.java
@@ -1,51 +1,79 @@
-package com.group_4_trial_1.Nutri_App_user_Trial.controller;
+package com.group_4_trial_1.Nutri_App_user_Trial.service;
 
 import com.group_4_trial_1.Nutri_App_user_Trial.entity.NutritionPlan;
-import com.group_4_trial_1.Nutri_App_user_Trial.service.NutritionPlanService;
+import com.group_4_trial_1.Nutri_App_user_Trial.repository.NutritionPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 
-@RestController
-@RequestMapping(path = "nutritionplan")
-public class NutritionPlanContoller {
 
-    private final NutritionPlanService nutritionPlanService;
+@Service
+public class NutritionPlanService {
+
+//  private Logger logger;
+    private final NutritionPlanRepository nutritionPlanRepository;
+
+    public NutritionPlanService(NutritionPlanRepository nutritionPlanRepository) {
+        this.nutritionPlanRepository = nutritionPlanRepository;
+    }
 
     @Autowired
-    public NutritionPlanContoller(NutritionPlanService nutritionPlanService) {
-        this.nutritionPlanService = nutritionPlanService;
+    public List<NutritionPlan> listAllPlans(){
+    //	logger.info("Getting all Nutrition Plans...");
+        return nutritionPlanRepository.findAll();
     }
 
-    @GetMapping
-    public List<NutritionPlan> getNutritionPlans(){
-   // throw new NutritionPlanApiRequestException("Oops cannot get all Nutrition Plans with custom exception");
-    //throw new IllegalStateException("Oops cannot get all Nutrition Plans");
-       return  nutritionPlanService.getNutritionPlans();
+    public NutritionPlan createPlan(NutritionPlan nutritionPlan) {
+   /*    Optional<NutritionPlanDTO> findNutritionPlanDTOByEmail = nutritionPlanRepository.
+                findNutritionPlanDTOByEmail(nutritionPlanDTO.getEmail())
+        if(nutritionPlanDTOOptional.isPresent()){
+            throw new IllegalStateException("Email Taken");
+        }*/
+    	return nutritionPlanRepository.save(nutritionPlan);
+//        System.out.println(nutritionPlan);
+       
     }
 
-    @PostMapping
-    public void registerNewNutritionPlan(@RequestBody NutritionPlan nutritionPlan){
-        nutritionPlanService.addNewNutritionPlan(nutritionPlan);
+
+    public void removePlan(Long nutritionPlanId) {
+         boolean exists = nutritionPlanRepository.existsById(nutritionPlanId);
+         if(!exists){
+             throw new IllegalStateException("Nutrition Plan for id "
+                     + nutritionPlanId + " does not exits");
+         }
+         nutritionPlanRepository.deleteById(nutritionPlanId);
     }
-    @DeleteMapping(path = "{nutritionPlanDTOId}")
-    public void deleteNutritionPlanDTO(
-            @PathVariable("nutritionPlanDTOId") Long nutritionPlanDTOId){
-        nutritionPlanService.deleteNutritionPlan(nutritionPlanDTOId);
-    }
 
-    @PutMapping(path = "{nutritionPlanDTOId}")
-    public void updateNutritionPlanDTO(
-            @PathVariable("nutritionPlanDTOId") Long nutritionPlanDTOId ,
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String planDiscription,
-
-           // @RequestParam(required = false) LocalDate created_At,
-            //@RequestParam(required = false) LocalDate updated_At,
-
-            @RequestParam(required = false) Double price) {
-        nutritionPlanService.updateNutritionPlan(nutritionPlanDTOId ,name
-                ,price,planDiscription);
+    @Transactional
+    public NutritionPlan changePlan(Long nutritionPlanId,
+                                       String name,
+                                       Double price , String planDiscription ) {
+        NutritionPlan nutritionPlanDTO = nutritionPlanRepository.findById(nutritionPlanId).
+                orElseThrow(() -> new IllegalStateException(
+                        "Nutrition Plan with id " + nutritionPlanId + " does not exists"));
+                if(name != null && name.length() > 0 &&
+                !Objects.equals(nutritionPlanDTO.getName(),name)){
+                    nutritionPlanDTO.setName(name);
+                }
+        if(price!= null  &&
+                !Objects.equals(nutritionPlanDTO.getPrice(),price)){
+            nutritionPlanDTO.setPrice(price);
+        }
+        if(planDiscription!= null  &&
+                !Objects.equals(nutritionPlanDTO.getPlanDiscription(),planDiscription)){
+            nutritionPlanDTO.setPlanDiscription(planDiscription);
+        }
+       /*  if(created_At!= null  &&
+                !Objects.equals(nutritionPlanDTO.getCreated_At(),created_At)){
+            nutritionPlanDTO.setCreated_At(created_At);
+        }
+        if(updated_At!= null  &&
+                !Objects.equals(nutritionPlanDTO.getUpdated_At(),updated_At)){
+            nutritionPlanDTO.setUpdated_At(updated_At);
+        }*/
+        return nutritionPlanDTO;
     }
 }
