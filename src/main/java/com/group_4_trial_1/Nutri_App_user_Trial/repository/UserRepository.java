2032bd1915ccacc15f4b3c16521cd7cc1d62e8ca
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-19 09:41:41 +0530

    Adding Documentation to UserRepository

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/UserRepository.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/repository/UserRepository.java
@@ -3,9 +3,20 @@ package com.group_4_trial_1.Nutri_App_user_Trial.repository;
 import com.group_4_trial_1.Nutri_App_user_Trial.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/*
+ * Author : Pratik
+ * Version : 1.0
+ * Date : 19-10-2022
+ * Description : This is UserRepository.
+ */
+
+@Repository
+/*Repository annotation is used to indicate that the class provides the mechanism
+ * for storage, retrieval, search, update and delete operation on objects*/
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // Postgres query -> SELECT user from user_table WHERE user_identification = ?;
