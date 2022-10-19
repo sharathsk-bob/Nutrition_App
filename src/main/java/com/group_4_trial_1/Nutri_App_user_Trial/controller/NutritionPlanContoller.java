4f67d05a76e56bb553b37a46539d11346de23923
MK-2000 <57285315+MK-2000@users.noreply.github.com>
2022-10-19 09:21:56 +0530

    Added Documentation

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/controller/NutritionPlanContoller.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/controller/NutritionPlanContoller.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Description : This is Nutrition Plan Controller
+
 @RestController
 @RequestMapping(path = "nutritionplan")
+/* Spring RestController takes care of mapping request data 
+ to the defined request handler method */ 
 public class NutritionPlanContoller {
 
     private final NutritionPlanServiceImpl nutritionPlanService;
@@ -19,23 +23,54 @@ public class NutritionPlanContoller {
         this.nutritionPlanService = nutritionPlanService;
     }
 
+    /************************************************************************************
+	 * Method: listAllPlans
+	 * Description: It is used to show all Nutrition Plans  from nutrition_plan  table
+	 * @returns flat :It returns nutrition_plan table with details
+	 * @GetMapping: It is used to handle the HTTP GET requests matched.
+	 *  
+	 ************************************************************************************/
     @GetMapping
     public List<NutritionPlan> listAllPlans(){
    // throw new NutritionPlanApiRequestException("Oops cannot get all Nutrition Plans with custom exception");
     //throw new IllegalStateException("Oops cannot get all Nutrition Plans");
        return  nutritionPlanService.getNutritionPlans();
     }
+    
+    /************************************************************************************
+	 * Method: createPlan
+	 * Description: It is used to add nutrition plan into nutrition_plan table
+	 * @PostMapping: It is used to handle the HTTP POST requests matched.
+	 * @RequestBody: It used to bind the HTTP request/response body with a domain object in method parameter or return type.
+	 * 
+	 ************************************************************************************/
 
     @PostMapping
     public void createPlan(@RequestBody NutritionPlan nutritionPlan){
         nutritionPlanService.addNewNutritionPlan(nutritionPlan);
     }
+    
+      /************************************************************************************
+	 * Method: removePlan
+	 * Description: It is used to remove Nutrition Plan from nutrition_plan table
+	 * @DeleteMapping: It is used to handle the HTTP DELETE requests matched.
+	 *  
+	 ************************************************************************************/
+    
     @DeleteMapping(path = "{nutritionPlanDTOId}")
     public void removePlan(
             @PathVariable("nutritionPlanDTOId") Long nutritionPlanDTOId){
         nutritionPlanService.deleteNutritionPlan(nutritionPlanDTOId);
     }
 
+    /************************************************************************************
+	 * Method: changePlan
+	 * Description: It is used to update nutrition Plan which is already there in nutrition_plan table
+	 * @PutMapping: It is used to handle the HTTP PUT requests matched.
+	 * @RequestBody: It used to bind the HTTP request/response body with a domain object in method parameter or return type.
+	 * 
+	 ************************************************************************************/
+    
     @PutMapping(path = "{nutritionPlanDTOId}")
     public void changePlan(
             @PathVariable("nutritionPlanDTOId") Long nutritionPlanDTOId ,
