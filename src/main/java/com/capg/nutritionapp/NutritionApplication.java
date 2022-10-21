3dfc4fd56125bb4154b72358bcd911c7c05a7e58
Pratik Kamble <pratikkambleat@gmail.com>
2022-10-21 12:47:28 +0530

    .

--- src/main/java/com/capg/nutritionapp/NutritionApplication.java
+++ src/main/java/com/capg/nutritionapp/NutritionApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EntityScan
-//@RestController
 public class NutritionApplication {
 
 	public static void main(String[] args) {
