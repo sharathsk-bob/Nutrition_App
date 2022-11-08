bad044b58ecf72ce1bd9866cb7f79a25ae211951
Tanya997 <taanya997@gmail.com>
2022-11-08 21:59:50 +0530

    Update PaymentDTO.java
    PaymentDto updated

--- src/main/java/com/capg/nutritionapp/dto/PaymentDTO.java
+++ src/main/java/com/capg/nutritionapp/dto/PaymentDTO.java
@@ -1,16 +1,32 @@
 package com.capg.nutritionapp.dto;
 
 import java.time.LocalDate;
+import java.util.Objects;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.capg.nutritionapp.entity.Payment;
 
 public class PaymentDTO {
+	// Data Fields
+	@NotNull(message="Id should not ne empty")
+	@Size(min=3,max=5, message="Id should be between 3 to 5 characters")
+	@Min(value=100, message="Id should not be less than 100")
 	private long id;
+	@NotNull(message="payment should not be empty")
     private float payment;
+	@NotNull(message="discount should not be empty")
     private float discount;
+	@NotNull(message="Creation date should not be empty")
     private LocalDate created_At;
+	@NotNull(message="Update date should not be empty")
     private LocalDate updated_At;
+	//relationships
     private String userId;
     private long planId;
     
+	// Constructors
     public PaymentDTO() {
     	super();
     }
@@ -25,6 +41,7 @@ public class PaymentDTO {
         this.planId = planId;
     }
 
+    //Getters Setters
     public long getId() {
 		return id;
 	}
@@ -85,11 +102,43 @@ public class PaymentDTO {
         this.planId = planId;
     }
 
+    //Overrides
 	@Override
 	public String toString() {
 		return "PaymentDTO [id=" + id + ", payment=" + payment + ", discount=" + discount + ", created_At=" + created_At
 				+ ", updated_At=" + updated_At + ", userId=" + userId + ", planId=" + planId + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(created_At, discount, id, payment, planId, updated_At, userId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PaymentDTO other = (PaymentDTO) obj;
+		return Objects.equals(created_At, other.created_At)
+				&& Float.floatToIntBits(discount) == Float.floatToIntBits(other.discount) && id == other.id
+				&& Float.floatToIntBits(payment) == Float.floatToIntBits(other.payment) && planId == other.planId
+				&& Objects.equals(updated_At, other.updated_At) && Objects.equals(userId, other.userId);
+	}
    
+	//Conversions
+	public Payment toPayment() {
+		Payment payment = new Payment();
+		payment.setId(this.getId());
+		payment.setPayment(this.getPayment());
+		payment.setDiscount(this.getDiscount());
+		payment.setCreated_At(this.getCreated_At());
+		payment.setUpdated_At(this.getUpdated_At());
+		payment.setUserId(this.getUserId());
+		payment.setPlanId(this.getPlanId());
+		return payment;
+	}
 }
